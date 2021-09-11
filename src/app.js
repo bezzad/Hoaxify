bd51fe7cb69fdabc063c31fd4b37f8338b9c11ef
Behzad <behzad.khosravifar@gmail.com>
2021-09-11 22:48:29 +0430

    Added perttier and nodemon packages

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 app.listen(3000, listenerCallback);
 
 function listenerCallback() {
-  console.log("App is running on Port: 3000");
+  console.log('App is running on Port: 3000');
 }
